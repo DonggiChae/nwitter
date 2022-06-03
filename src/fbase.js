98e9943a72ebabebdeb36a6275aab55bb37693b0
Dongi <chaedonggi@gmail.com>
2022-06-03 13:16:07 +0900

    Using Firebase Auth

--- src/fbase.js
+++ src/fbase.js
@@ -1,5 +1,6 @@
 
-import { initializeApp } from "firebase/app";
+import * as firebase from "firebase/app";
+import "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -11,8 +12,7 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
+firebase.initializeApp(firebaseConfig);
 
-export default firebase.initializeApp(firebaseConfig);
+export const authService = firebase.auth();
